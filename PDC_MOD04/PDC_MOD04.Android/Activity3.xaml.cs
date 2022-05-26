9d254480a7d980e761900b31b874ec66aa07b513
Earl Wendell Nakpil <nakpil.earlwendell@auf.edu.ph>
2022-05-26 23:08:19 +0800

    Add project files.

---
+++ PDC_MOD04/PDC_MOD04.Android/Activity3.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using System.Collections.ObjectModel;
+
+namespace PDC_MOD04
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Activity3 : ContentPage
+    {
+        ObservableCollection<Employee> employees = new ObservableCollection<Employee>();
+
+        public ObservableCollection<Employee> Employees { get { return employees; } }
+        public Activity3()
+        {
+            InitializeComponent();
+
+            EmployeeView.ItemsSource = employees;
+
+            employees.Add(new Employee { DisplayName = "Twice Nayeon", position = "Lead Vocalist", Image = "nayeon1.jpg" });
+            employees.Add(new Employee { DisplayName = "Twice Nayeon", position = "Lead Vocalist", Image = "nayeon2.jpg" });
+            employees.Add(new Employee { DisplayName = "Twice Nayeon", position = "Lead Vocalist", Image = "nayeon3.jpg" });
+            employees.Add(new Employee { DisplayName = "Twice Nayeon", position = "Lead Vocalist", Image = "nayeon4.jpg" });
+            employees.Add(new Employee { DisplayName = "Twice Nayeon", position = "Lead Vocalist", Image = "nayeon5.jpg" });
+        }
+    }
+}
